61e403dfd202ad73f910994ed890cc3901b65602
NorejOn <oreonorej@gmail.com>
2023-04-08 15:07:13 +0300

    Problem in hw3 security

--- homework1/src/main/java/com/example/homework1/dao/ClientUserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.homework1.dao;
-
-import com.example.homework1.models.ClientUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientUserDAO extends JpaRepository<ClientUser,Integer> {
-    ClientUser findByEmail(String email);
-}
--- homework1/src/main/java/com/example/homework1/models/Role.java
+++
@@ -1,5 +0,0 @@
-package com.example.homework1.models;
-
-public enum Role {
-    USER,ADMIN
-}
